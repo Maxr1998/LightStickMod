436ee069cc85e4573e499ace51b1edec6e87f0b2
Maxr1998 <max.rumpf1998@gmail.com>
2022-08-17 20:56:33 +0200

    Refactor colors and move global variable to static variable

--- firmware/loona_colors.h
+++ firmware/loona_colors.h
@@ -1,16 +1,20 @@
-#include "esphome.h"
+#include "esphome/core/log.h"
+#include "esphome/components/light/light_state.h"
+#include "esphome/components/light/light_call.h"
 
-void go_to_next_color() {
-    auto call = id(leds).turn_on();
+void go_to_next_color(esphome::light::LightState *light) {
+    static int current_member = 0;
+
+    auto call = light->turn_on();
     call.set_transition_length(300);
     call.set_brightness(1.0);
-    if (id(current_member) < 12) {
+    if (current_member < 12) {
         // Starts at 0 on boot, will be at least 1 and at most 12
-        id(current_member)++;
+        current_member++;
     } else {
-        id(current_member) = 1;
+        current_member = 1;
     }
-    switch (id(current_member)) {
+    switch (current_member) {
         case 1: // HeeJin
             call.set_rgb(0.871, 0.2, 0.451);
             break;
@@ -47,10 +51,14 @@ void go_to_next_color() {
         case 12: // Olivia Hye
             call.set_rgb(0.753, 0.761, 0.761);
             break;
+        default:
+            call.set_rgb(1.0, 1.0, 1.0);
+            current_member = 0;
+            break;
     }
     call.set_white(0.0);
     call.set_effect("none"); // reset effect
     call.perform();
 
-    ESP_LOGI("loona_colors", "Changed color to member %d", id(current_member));
+    ESP_LOGI("loona_colors", "Changed color to member %d", current_member);
 }
